74b7c8a7e5892290602d2f73f97981ccd4ac9335
xdo13 <xdo13@naver.com>
2024-12-27 13:02:08 +0900

    새로 만들기 창 틀 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,4 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import Header from "../component/Header";
+import Button from "../component/Button";
+import Editor from "../component/Editor";
+
 const New = () =>{
-    return<div>New 페이지입니다</div>;
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    };
+    return(
+        <div>
+            <Header
+                title={"새 일기 쓰기"}
+                leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+            />
+            <Editor />
+        </div>
+    );
 };
 export default New;
